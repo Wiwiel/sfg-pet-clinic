40391090f90060960daa289d2dd4c0adcbd14967
Wiwiel <vera,y,zhukova@gmail.com>
2023-01-07 13:30:59 +0400

    Refactoring to common crud interface. Closes #10.

--- pet-clinic-data/src/main/java/wiwiel/training/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/wiwiel/training/sfgpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package wiwiel.training.sfgpetclinic.services;
 
 import wiwiel.training.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
     Vet findByLastName(String lastName);
-
-    Vet findById (Long id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
 }

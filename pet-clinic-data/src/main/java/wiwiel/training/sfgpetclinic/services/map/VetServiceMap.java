e7160b2958e76da4e039c6171083aa47c06dca1d
Wiwiel <vera,y,zhukova@gmail.com>
2023-01-07 14:27:34 +0400

    Impl Map based Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/wiwiel/training/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package wiwiel.training.sfgpetclinic.services.map;
+
+import wiwiel.training.sfgpetclinic.model.Vet;
+import wiwiel.training.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

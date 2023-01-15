da3877e93b47785b32e03c33a3311ad5748a021e
Wiwiel <vera,y,zhukova@gmail.com>
2023-01-15 15:41:41 +0400

    Added PetTypes to bootstrap. Closes #30.

--- pet-clinic-web/src/main/java/wiwiel/training/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/wiwiel/training/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package wiwiel.training.sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import wiwiel.training.sfgpetclinic.model.Owner;
+import wiwiel.training.sfgpetclinic.model.PetType;
 import wiwiel.training.sfgpetclinic.model.Vet;
 import wiwiel.training.sfgpetclinic.services.OwnerService;
+import wiwiel.training.sfgpetclinic.services.PetTypeService;
 import wiwiel.training.sfgpetclinic.services.VetService;
 
 @Component
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");

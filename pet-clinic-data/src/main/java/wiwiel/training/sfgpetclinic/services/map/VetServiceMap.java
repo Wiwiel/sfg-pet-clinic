7e0056f82a848597d726f894569245dabb51be2c
Wiwiel <vera,y,zhukova@gmail.com>
2023-01-15 16:50:48 +0400

    Added Specialities to Vets on startup. Closes #32.

--- pet-clinic-data/src/main/java/wiwiel/training/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/wiwiel/training/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,13 +1,22 @@
 package wiwiel.training.sfgpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
+import wiwiel.training.sfgpetclinic.model.Speciality;
 import wiwiel.training.sfgpetclinic.model.Vet;
+import wiwiel.training.sfgpetclinic.services.SpecialityService;
 import wiwiel.training.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,7 +29,16 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object);
+
+        if(object.getSpecialities().size()>0){
+            object.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null){
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
+            return super.save(object);
     }
 
     @Override

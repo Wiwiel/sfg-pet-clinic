bbb81f1470aaf5a115d545538fc500f14ed82051
Wiwiel <vera,y,zhukova@gmail.com>
2023-01-15 16:21:32 +0400

    Added Specialty Map Service Impl. Closes #29.

---
+++ pet-clinic-data/src/main/java/wiwiel/training/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package wiwiel.training.sfgpetclinic.services;
+
+import wiwiel.training.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
